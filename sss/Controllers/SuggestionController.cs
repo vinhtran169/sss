a6d7e0a8afc2d69ce4b5a89f4b966b861df37b29
Nikkei <nhatkhanh2311@gmail.com>
2021-06-24 17:36:45 +0700

    khanh 20210624 5pm: list suggestion

---
+++ sss/Controllers/SuggestionController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sss.Models;
+
+namespace sss.Controllers
+{
+    public class SuggestionController : Controller
+    {
+        [Route("home/suggest/list")]
+        public IActionResult ListSuggestion()
+        {
+            HttpContext.Session.SetString("username", "admin"); //temp session
+            string username = HttpContext.Session.GetString("username");
+
+            if (username != null)
+                using (sssContext dbContext = new sssContext())
+                {
+                    var listSuggest = dbContext.Suggestions.ToList();
+                    return View(listSuggest);
+                }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public IActionResult EditSuggestion()
+        {
+            return View();
+        }
+        
+        public IActionResult DetailsSuggestion()
+        {
+            return View();
+        }
+        
+        public IActionResult DeleteSuggestion()
+        {
+            return View();
+        }
+    }
+}
