b14712463664620d303eb1623c37066aec85dadd
cong <vtcong551999@gmail.com>
2021-06-24 16:18:31 +0700

    design master page

--- sss/Models/sssContext.cs
+++ sss/Models/sssContext.cs
@@ -25,7 +25,7 @@ namespace sss.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=THANH_CONG\\SQLEXPRESS;Initial Catalog=sss;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer("Server=THANH_CONG\\SQLEXPRESS;Database=sss;User Id=sa;password=123456;Trusted_Connection=False;");
             }
         }
 
