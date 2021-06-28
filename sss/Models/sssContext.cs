305dee55271f4f39be3899908383c2efcaedfaf9
cong <vtcong551999@gmail.com>
2021-06-28 22:01:28 +0700

    t_cong_frontend 20210628 2200: update layout page

--- sss/Models/sssContext.cs
+++ sss/Models/sssContext.cs
@@ -27,7 +27,7 @@ namespace sss.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=THANH_CONG\\SQLEXPRESS;Database=sss;User Id=sa;password=123456;Trusted_Connection=False;");
+                optionsBuilder.UseSqlServer("Server=TVVINH-NEWPC\\SQLEXPRESS;Database=sss;User Id=sa;password=khongco@12;Trusted_Connection=False;");
             }
         }
 
