d2794bde4ab6917c2f2505d346fded48302fb5dd
cong <vtcong551999@gmail.com>
2021-07-19 08:15:37 +0700

    vt_cong_frontend 20210719 8:15 : Login form

---
+++ sss/Controllers/SystemuserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using sss.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sss.Controllers
+{
+	public class SystemuserController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+
+        [HttpPost]
+        public IActionResult Login(IFormCollection form)
+        {
+            string username = form["username"];
+            string password = form["password"];
+
+            using (sssContext dbContext = new sssContext())
+            {
+                bool existUser = dbContext.Systemusers.Any(user => user.Username == username && user.Password == password);
+
+                if (existUser)
+                {
+                    HttpContext.Session.SetString("username", username);
+                    //return Redirect("~/");
+                    return View("Login");
+                }
+                else
+                {
+                    ViewBag.error = existUser;
+                    return View("Login");
+                }
+            }
+        }
+    }
+}
